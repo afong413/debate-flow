d8b4d8d57ef7653b592cb6a56fc05ac38713c67b
Aiden Fong <afong413@gmail.com>
2024-10-14 23:35:33 -0700

    Add Vercel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwind from "@astrojs/tailwind"
 
 import solidJs from "@astrojs/solid-js"
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  output: "server",
+  adapter: vercel(),
 })
